f107a6f0af82525a6a3af641b14a9d386d0933c6
Aaron VonderHaar <gruen0aermel@gmail.com>
2011-08-22 03:14:39 -0700

    Improve memory usage: only load one copy of each image resource

--- src/main/java/net/avh4/framework/uilayer/scene/SwingImage.java
+++ src/main/java/net/avh4/framework/uilayer/scene/SwingImage.java
@@ -3,6 +3,7 @@ package net.avh4.framework.uilayer.scene;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URL;
+import java.util.HashMap;
 
 import javax.imageio.ImageIO;
 
@@ -29,27 +30,37 @@ class SwingImage {
 
 	private final int width;
 	private final int height;
-	private final URL resource;
 	private final BufferedImage image;
+	private static final HashMap<String, BufferedImage> cache = new HashMap<String, BufferedImage>();
 
 	/**
 	 * @param imageName
 	 *            The image specified must exist on the classpath in the default
 	 *            package.
 	 */
-	public SwingImage(final int x, final int y, final int width, final int height,
-			final String imageName) {
+	public SwingImage(final int x, final int y, final int width,
+			final int height, final String imageName) {
 		this.x = x;
 		this.y = y;
 		this.width = width;
 		this.height = height;
-		resource = ClassLoader.getSystemResource(imageName);
+		image = loadImage(imageName);
+	}
+
+	private BufferedImage loadImage(final String imageName) {
+		if (cache.containsKey(imageName)) {
+			return cache.get(imageName);
+		}
+
+		final URL resource = ClassLoader.getSystemResource(imageName);
 		if (resource == null) {
 			throw new IllegalArgumentException(String.format(
 					"No such resource '%s'", imageName));
 		}
 		try {
-			image = ImageIO.read(resource);
+			final BufferedImage image = ImageIO.read(resource);
+			cache.put(imageName, image);
+			return image;
 		} catch (final IOException e) {
 			e.printStackTrace();
 			throw new IllegalArgumentException(String.format(
