ff50c47efcd8343a909d9e6b43f2fde86c9ee50d
Aaron VonderHaar <gruen0aermel@gmail.com>
2011-08-22 02:36:10 -0700

    Rename Scenegraph to Scene for simplicity

--- src/main/java/net/avh4/demo/uilayer/DemoGame.java
+++ src/main/java/net/avh4/demo/uilayer/DemoGame.java
@@ -1,23 +1,23 @@
 package net.avh4.demo.uilayer;
 
-import net.avh4.framework.uilayer.ScenegraphCreator;
+import net.avh4.framework.uilayer.SceneCreator;
 import net.avh4.framework.uilayer.UILayer;
-import net.avh4.framework.uilayer.model.Scenegraph;
+import net.avh4.framework.uilayer.scene.Scene;
 
-public class DemoGame implements ScenegraphCreator {
+public class DemoGame implements SceneCreator {
 
 	public static void main(final String[] args) {
 		UILayer.main(new DemoGame());
 	}
 
-	private final Scenegraph s;
+	private final Scene s;
 
 	public DemoGame() {
-		s = new Scenegraph("UILayer Demo Game");
+		s = new Scene("UILayer Demo Game");
 		s.addImage(0, 0, 800, 600, "background.jpg");
 	}
 
-	public Scenegraph getScenegraph() {
+	public Scene getScene() {
 		return s;
 	}
 }
