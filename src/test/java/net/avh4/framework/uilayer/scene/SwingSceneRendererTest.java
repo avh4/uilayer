edd2c6c24a67d59d973d9a4a0fd723890ae2c0f8
Aaron VonderHaar <gruen0aermel@gmail.com>
2011-09-06 22:42:17 -0700

    Cleanup variable names

--- src/test/java/net/avh4/framework/uilayer/scene/SwingSceneRendererTest.java
+++ src/test/java/net/avh4/framework/uilayer/scene/SwingSceneRendererTest.java
@@ -20,13 +20,13 @@ public class SwingSceneRendererTest {
 
 	private SceneCreator mockCreator;
 	private SwingSceneRenderer subject;
-	private Scene scenegraph;
+	private Scene scene;
 
 	@Before
 	public void setUp() {
-		scenegraph = new Scene();
+		scene = new Scene();
 		mockCreator = mock(SceneCreator.class);
-		stub(mockCreator.getScene()).toReturn(scenegraph);
+		stub(mockCreator.getScene()).toReturn(scene);
 		subject = new SwingSceneRenderer(mockCreator);
 	}
 
@@ -37,7 +37,7 @@ public class SwingSceneRendererTest {
 
 	@Test
 	public void testRenderResourceImage() throws Exception {
-		scenegraph.addImage(100, 100, 50, 50, "tile1.png");
+		scene.addImage(100, 100, 50, 50, "tile1.png");
 		assertThat(subject, isApproved());
 	}
 
@@ -52,15 +52,15 @@ public class SwingSceneRendererTest {
 		g.fillRect(10, 10, 50, 50);
 		g.dispose();
 
-		scenegraph.addImage(100, 100, 50, 50, image);
+		scene.addImage(100, 100, 50, 50, image);
 		assertThat(subject, isApproved());
 	}
 
 	@Test
 	public void testRenderMultipleImages() throws Exception {
-		scenegraph.addImage(100, 100, 150, 150, "tile1.png");
-		scenegraph.addImage(150, 150, 75, 75, "tile2.png");
-		scenegraph.addImage(100, 300, 16, 16, "tile1.png");
+		scene.addImage(100, 100, 150, 150, "tile1.png");
+		scene.addImage(150, 150, 75, 75, "tile2.png");
+		scene.addImage(100, 300, 16, 16, "tile1.png");
 		assertThat(subject, isApproved());
 	}
 
@@ -77,18 +77,18 @@ public class SwingSceneRendererTest {
 
 	@Test
 	public void testRenderPlaceholders() throws Exception {
-		scenegraph.addPlaceholder("Background", 0, 0, 800, 600);
-		scenegraph.addPlaceholder("Body", 20, 20, 100, 560);
+		scene.addPlaceholder("Background", 0, 0, 800, 600);
+		scene.addPlaceholder("Body", 20, 20, 100, 560);
 		assertThat(subject, isApproved());
 	}
 
 	@Test
 	public void testRenderText() throws Exception {
 		final String longString = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
-		scenegraph.addPlaceholder("Text box: size 36", 50, 50, 200, 1000);
-		scenegraph.addText(longString, 50, 50, 200, "Tuffy.ttf", 36);
-		scenegraph.addPlaceholder("Text box: size 12", 450, 50, 200, 1000);
-		scenegraph.addText(longString, 450, 50, 200, "Tuffy.ttf", 12);
+		scene.addPlaceholder("Text box: size 36", 50, 50, 200, 1000);
+		scene.addText(longString, 50, 50, 200, "Tuffy.ttf", 36);
+		scene.addPlaceholder("Text box: size 12", 450, 50, 200, 1000);
+		scene.addText(longString, 450, 50, 200, "Tuffy.ttf", 12);
 		assertThat(subject, isApproved());
 	}
 
