85eec79fb57c1e1985fc76fb3ea8de07c796f9e2
Aaron VonderHaar <gruen0aermel@gmail.com>
2011-09-09 22:01:44 -0700

    Handle loading of AndroidUILayerService

--- src/main/java/net/avh4/framework/uilayer/UILayer.java
+++ src/main/java/net/avh4/framework/uilayer/UILayer.java
@@ -4,29 +4,45 @@ import net.avh4.framework.uilayer.scene.Scene;
 
 public class UILayer {
 
-	public static final UILayerService service = loadService();
 	private static final String SWING_SERVICE = "net.avh4.framework.uilayer.swing.SwingUILayerService";
+	private static final String ANDROID_SERVICE = "net.avh4.framework.uilayer.android.AndroidUILayerService";
+
+	private static final String[] KNOWN_SERVICES = new String[] {
+			SWING_SERVICE, ANDROID_SERVICE };
+
+	public static final UILayerService service = loadService();
 
 	public static void main(final UI game) {
 		UILayer.main(game, game, game);
 	}
 
 	private static UILayerService loadService() {
+		for (final String serviceName : KNOWN_SERVICES) {
+			final UILayerService service = loadService(serviceName);
+			if (service != null) {
+				return service;
+			}
+		}
+		throw new RuntimeException(
+				String.format(
+						"Could not find a known UILayerService on the classpath (checked for %s).  If you are using maven, your project must include uilayer-swing as a runtime dependency.",
+						(Object) KNOWN_SERVICES));
+	}
+
+	private static UILayerService loadService(final String serviceName) {
 		try {
 			final Class<?> clazz = UILayer.class.getClassLoader().loadClass(
-					SWING_SERVICE);
+					serviceName);
 			return (UILayerService) clazz.newInstance();
 		} catch (final ClassNotFoundException e) {
-			throw new RuntimeException(
-					String.format(
-							"Could not find %s on the classpath.  If you are using maven, your project must include uilayer-swing as a runtime dependency.",
-							SWING_SERVICE), e);
+			e.printStackTrace();
+			return null;
 		} catch (final InstantiationException e) {
 			throw new RuntimeException(String.format(
-					"Could not instantiate %s", SWING_SERVICE), e);
+					"Could not instantiate %s", serviceName), e);
 		} catch (final IllegalAccessException e) {
 			throw new RuntimeException(String.format(
-					"Could not instantiate %s", SWING_SERVICE), e);
+					"Could not instantiate %s", serviceName), e);
 		}
 	}
 
