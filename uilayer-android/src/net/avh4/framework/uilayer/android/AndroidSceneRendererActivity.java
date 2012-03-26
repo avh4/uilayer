9151e5f2065d8492090704ba0007430fdcec6077
Aaron VonderHaar <gruen0aermel@gmail.com>
2012-03-25 17:18:07 -0700

    Click support for uilayer-android

--- uilayer-android/src/net/avh4/framework/uilayer/android/AndroidSceneRendererActivity.java
+++ uilayer-android/src/net/avh4/framework/uilayer/android/AndroidSceneRendererActivity.java
@@ -2,6 +2,7 @@ package net.avh4.framework.uilayer.android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.Window;
 import android.widget.ImageView;
 import net.avh4.framework.uilayer.R;
@@ -11,7 +12,8 @@ import net.avh4.framework.uilayer.scene.AndroidSceneRenderer;
 
 public class AndroidSceneRendererActivity extends Activity {
 
-    private UI mUi;
+    private UI ui;
+    private AndroidSceneRenderer uiView;
 
     public AndroidSceneRendererActivity() {
         this(null);
@@ -20,7 +22,7 @@ public class AndroidSceneRendererActivity extends Activity {
     public AndroidSceneRendererActivity(final UI ui) {
         super();
         ((AndroidUILayerService) UILayer.service).init(this);
-        mUi = ui;
+        this.ui = ui;
     }
 
     /**
@@ -30,15 +32,25 @@ public class AndroidSceneRendererActivity extends Activity {
     public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setUI(mUi);
+        setUI(ui);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_UP) {
+            ui.click((int) event.getX(), (int) event.getY());
+            uiView.invalidate();
+        }
+        return true;
     }
 
     public void setUI(final UI ui) {
-        mUi = ui;
+        this.ui = ui;
         runOnUiThread(new Runnable() {
             public void run() {
-                if (mUi != null) {
-                    setContentView(new AndroidSceneRenderer(AndroidSceneRendererActivity.this, mUi));
+                if (AndroidSceneRendererActivity.this.ui != null) {
+                    uiView = new AndroidSceneRenderer(AndroidSceneRendererActivity.this, AndroidSceneRendererActivity.this.ui);
+                    setContentView(uiView);
                 } else {
                     final ImageView placeholder = new ImageView(
                             AndroidSceneRendererActivity.this);
--- uilayer-android/tests/src/net/avh4/framework/uilayer/android/AndroidSceneRendererActivityTest.java
+++ uilayer-android/tests/src/net/avh4/framework/uilayer/android/AndroidSceneRendererActivityTest.java
@@ -1,13 +1,41 @@
 package net.avh4.framework.uilayer.android;
 
-import android.app.Activity;
-import junit.framework.TestCase;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.TouchUtils;
+import net.avh4.framework.uilayer.UI;
 import net.avh4.framework.uilayer.UILayer;
+import org.easymock.EasyMock;
 
-public class AndroidSceneRendererActivityTest extends TestCase {
+public class AndroidSceneRendererActivityTest extends ActivityInstrumentationTestCase2<AndroidSceneRendererActivity> {
+
+    private AndroidSceneRendererActivity activity;
+    private UI ui;
+
+    public AndroidSceneRendererActivityTest() {
+        super(AndroidSceneRendererActivity.class);
+    }
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+
+        ui = EasyMock.createMock(UI.class);
+        EasyMock.expect(ui.getScene()).andStubReturn(null);
+
+        activity = getActivity();
+        activity.setUI(ui);
+    }
 
     public void testShouldSetServiceContext() throws Exception {
-        final Activity activity = new AndroidSceneRendererActivity();
         assertNotNull(((AndroidUILayerService) UILayer.service).context);
+    }
+
+    public void testDispatchClick() {
+        ui.click(360, 592);
+        EasyMock.replay(ui);
+
+        TouchUtils.clickView(this, activity.getWindow().getDecorView());
+
+        EasyMock.verify(ui);
     }
 }
