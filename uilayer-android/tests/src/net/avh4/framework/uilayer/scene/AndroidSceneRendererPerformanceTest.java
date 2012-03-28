946752d336d75a89f9c63e28ae64b324f50864e6
Aaron VonderHaar <gruen0aermel@gmail.com>
2012-03-27 19:52:50 -0700

    Performance test for AndroidSceneRenderer.drawText()

---
+++ uilayer-android/tests/src/net/avh4/framework/uilayer/scene/AndroidSceneRendererPerformanceTest.java
@@ -0,0 +1,48 @@
+package net.avh4.framework.uilayer.scene;
+
+import android.test.InstrumentationTestCase;
+import net.avh4.framework.uilayer.Color;
+import net.avh4.framework.uilayer.Font;
+import net.avh4.framework.uilayer.UI;
+import org.easymock.EasyMock;
+
+import static net.avh4.framework.uilayer.scene.PerformanceTestCanvas.TIME_TO_DRAW_TEXT;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.lessThan;
+
+public class AndroidSceneRendererPerformanceTest extends InstrumentationTestCase {
+
+    private Scene scene;
+    private AndroidSceneRenderer subject;
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        scene = new Scene();
+        UI ui = EasyMock.createMock(UI.class);
+        EasyMock.expect(ui.getScene()).andStubReturn(scene);
+        EasyMock.replay(ui);
+        subject = new AndroidSceneRenderer(getInstrumentation().getTargetContext(), ui);
+    }
+
+    public void testSceneTextPerformance() throws Exception {
+        final String stringOfAThousandWords = makeStringOfAThousandWords();
+        scene.add(new SceneText(stringOfAThousandWords, 0, 0, 720, Font.PFENNIG, Color.WHITE));
+
+        assertThat(measureRenderingTimeUnits(), lessThan(100 * TIME_TO_DRAW_TEXT));
+    }
+
+    private int measureRenderingTimeUnits() {
+        final PerformanceTestCanvas canvas = new PerformanceTestCanvas();
+        subject.onDraw(canvas);
+        return canvas.timeUnitsElapsed;
+    }
+
+    private String makeStringOfAThousandWords() {
+        final StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 1000; i++) {
+            sb.append("Word ");
+        }
+        return sb.toString();
+    }
+
+}
---
+++ uilayer-android/tests/src/net/avh4/framework/uilayer/scene/PerformanceTestCanvas.java
@@ -0,0 +1,54 @@
+package net.avh4.framework.uilayer.scene;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
+
+class PerformanceTestCanvas extends Canvas {
+    public int timeUnitsElapsed;
+    private static final int TIME_TO_DRAW_RECT = 1;
+    public static final int TIME_TO_DRAW_TEXT = 1;
+
+    @Override
+    public void drawRect(RectF rect, Paint paint) {
+        super.drawRect(rect, paint);
+        timeUnitsElapsed += TIME_TO_DRAW_RECT;
+    }
+
+    @Override
+    public void drawRect(Rect r, Paint paint) {
+        super.drawRect(r, paint);
+        timeUnitsElapsed += TIME_TO_DRAW_RECT;
+    }
+
+    @Override
+    public void drawRect(float left, float top, float right, float bottom, Paint paint) {
+        super.drawRect(left, top, right, bottom, paint);
+        timeUnitsElapsed += TIME_TO_DRAW_RECT;
+    }
+
+    @Override
+    public void drawText(String text, float x, float y, Paint paint) {
+        super.drawText(text, x, y, paint);
+        timeUnitsElapsed += TIME_TO_DRAW_TEXT;
+    }
+
+    @Override
+    public void drawText(char[] text, int index, int count, float x, float y, Paint paint) {
+        super.drawText(text, index, count, x, y, paint);
+        timeUnitsElapsed += TIME_TO_DRAW_TEXT;
+    }
+
+    @Override
+    public void drawText(String text, int start, int end, float x, float y, Paint paint) {
+        super.drawText(text, start, end, x, y, paint);
+        timeUnitsElapsed += TIME_TO_DRAW_TEXT;
+    }
+
+    @Override
+    public void drawText(CharSequence text, int start, int end, float x, float y, Paint paint) {
+        super.drawText(text, start, end, x, y, paint);
+        timeUnitsElapsed += TIME_TO_DRAW_TEXT;
+    }
+}
