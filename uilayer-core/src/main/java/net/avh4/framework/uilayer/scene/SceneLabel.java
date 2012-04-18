94f7763306c8da3d7fd79d1f19a5e70be34acd3a
Aaron VonderHaar <gruen0aermel@gmail.com>
2012-04-17 23:04:45 -0700

    Remove SceneLabel.fontSize

--- uilayer-core/src/main/java/net/avh4/framework/uilayer/scene/SceneLabel.java
+++ uilayer-core/src/main/java/net/avh4/framework/uilayer/scene/SceneLabel.java
@@ -6,21 +6,19 @@ public class SceneLabel extends SceneElement {
 
     protected String text;
     protected final Font font;
-    protected final int fontSize;
     protected final int color;
 
     public SceneLabel(final String text, final int x, final int y,
-                      final Font font, final int fontSize, final int color) {
+                      final Font font, final int color) {
         super(text, x, y, 1, 1);
         this.text = text;
         this.color = color;
         this.font = font;
-        this.fontSize = fontSize;
     }
 
     public SceneLabel(final String text, final int x, final int y,
                       final String customFontResource, final int fontSize, final int color) {
-        this(text, x, y, new Font(customFontResource), fontSize, color);
+        this(text, x, y, new Font(customFontResource).size(fontSize), color);
     }
 
 }
