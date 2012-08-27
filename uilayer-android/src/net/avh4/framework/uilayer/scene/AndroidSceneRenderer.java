650ab57b90a3ad41731fd3fe735ffb4b996d767c
Aaron VonderHaar <gruen0aermel@gmail.com>
2012-08-26 18:01:35 -0700

    Fix for uilayer-android: Move drawing of SceneElements out of SceneRenderer into the SceneElement subclasses.

--- uilayer-android/src/net/avh4/framework/uilayer/scene/AndroidSceneRenderer.java
+++ uilayer-android/src/net/avh4/framework/uilayer/scene/AndroidSceneRenderer.java
@@ -1,30 +1,32 @@
 package net.avh4.framework.uilayer.scene;
 
+import net.avh4.framework.uilayer.SceneCreator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.view.View;
-import net.avh4.framework.uilayer.SceneCreator;
 
 public class AndroidSceneRenderer extends View {
-    private final AndroidGraphicsOperations graphicsOperations;
-    private final SceneRenderer renderer;
+	private final AndroidGraphicsOperations graphicsOperations;
+	private final AndroidFontMetricsService fontMetricsService;
+	private final SceneRenderer renderer;
 
-    public AndroidSceneRenderer(final Context context, final SceneCreator creator) {
-        super(context);
-        if (creator == null) {
-            throw new RuntimeException("SceneCreator must not be null");
-        }
-        final AndroidFontMetricsService fm = new AndroidFontMetricsService(context);
-        graphicsOperations = new AndroidGraphicsOperations(context);
-        renderer = new SceneRenderer(creator, graphicsOperations, fm);
-    }
+	public AndroidSceneRenderer(final Context context,
+			final SceneCreator creator) {
+		super(context);
+		if (creator == null) {
+			throw new RuntimeException("SceneCreator must not be null");
+		}
+		fontMetricsService = new AndroidFontMetricsService(context);
+		graphicsOperations = new AndroidGraphicsOperations(context);
+		renderer = new SceneRenderer(creator);
+	}
 
-    @Override
-    protected void onDraw(final Canvas canvas) {
-        synchronized (graphicsOperations) {
-            graphicsOperations.setCanvas(canvas);
-            renderer.render();
-            graphicsOperations.setCanvas(null);
-        }
-    }
+	@Override
+	protected void onDraw(final Canvas canvas) {
+		synchronized (graphicsOperations) {
+			graphicsOperations.setCanvas(canvas);
+			renderer.render(graphicsOperations, fontMetricsService);
+			graphicsOperations.setCanvas(null);
+		}
+	}
 }
