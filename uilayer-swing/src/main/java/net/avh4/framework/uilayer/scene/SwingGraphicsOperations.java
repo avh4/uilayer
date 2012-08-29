16aca17e7e25b8a56da94693d1b56f1770da5103
Aaron VonderHaar <gruen0aermel@gmail.com>
2012-08-28 18:56:05 -0700

    Enable antialiasing in SwingSceneRenderer.

--- uilayer-swing/src/main/java/net/avh4/framework/uilayer/scene/SwingGraphicsOperations.java
+++ uilayer-swing/src/main/java/net/avh4/framework/uilayer/scene/SwingGraphicsOperations.java
@@ -46,6 +46,12 @@ public class SwingGraphicsOperations implements GraphicsOperations {
     }
 
     public void setGraphicsContext(Graphics g) {
+        if (g instanceof Graphics2D) {
+            final Graphics2D g2 = (Graphics2D) g;
+            System.out.println("Alias");
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        }
         this.g = g;
     }
 
