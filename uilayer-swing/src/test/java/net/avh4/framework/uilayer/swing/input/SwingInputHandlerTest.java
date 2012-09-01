284e7231a20304fab9d94efffb1ae9053d03a98d
Aaron VonderHaar <gruen0aermel@gmail.com>
2012-09-01 13:24:26 -0700

    Reformat SwingInputHandlerTest.

--- uilayer-swing/src/test/java/net/avh4/framework/uilayer/swing/input/SwingInputHandlerTest.java
+++ uilayer-swing/src/test/java/net/avh4/framework/uilayer/swing/input/SwingInputHandlerTest.java
@@ -19,62 +19,62 @@ import static org.mockito.Mockito.verify;
 
 public class SwingInputHandlerTest {
 
-	private JPanel dummyEventSource;
-	private ClickReceiver clickReceiver;
-	private KeyReceiver keyReceiver;
-	private SwingInputHandler subject;
+    private JPanel dummyEventSource;
+    private ClickReceiver clickReceiver;
+    private KeyReceiver keyReceiver;
+    private SwingInputHandler subject;
 
     @BeforeClass
     public static void setUpOnce() {
         FailOnThreadViolationRepaintManager.install();
     }
 
-	@Before
-	public void setUp() {
+    @Before
+    public void setUp() {
         dummyEventSource = GuiActionRunner.execute(new GuiQuery<JPanel>() {
             @Override
             protected JPanel executeInEDT() throws Throwable {
                 return new JPanel();
             }
         });
-		clickReceiver = mock(ClickReceiver.class);
-		keyReceiver = mock(KeyReceiver.class);
-		subject = new SwingInputHandler(clickReceiver, keyReceiver);
-	}
+        clickReceiver = mock(ClickReceiver.class);
+        keyReceiver = mock(KeyReceiver.class);
+        subject = new SwingInputHandler(clickReceiver, keyReceiver);
+    }
 
-	/**
-	 * Make sure that instantiating with null receivers does not cause
-	 * NullPointerExceptions.
-	 */
-	@Test
-	public void testInstanceWithNullReceivers() {
-		subject = new SwingInputHandler(null, null);
-		final MouseEvent me = new MouseEvent(dummyEventSource, 0, 0, 0, 100,
-				100, 1, false);
-		final KeyEvent ke = new KeyEvent(dummyEventSource, 0, 0, 0,
-				KeyEvent.VK_DOWN, '\0');
+    /**
+     * Make sure that instantiating with null receivers does not cause
+     * NullPointerExceptions.
+     */
+    @Test
+    public void testInstanceWithNullReceivers() {
+        subject = new SwingInputHandler(null, null);
+        final MouseEvent me = new MouseEvent(dummyEventSource, 0, 0, 0, 100,
+                100, 1, false);
+        final KeyEvent ke = new KeyEvent(dummyEventSource, 0, 0, 0,
+                KeyEvent.VK_DOWN, '\0');
 
-		subject.mouseClicked(me);
-		subject.keyPressed(ke);
+        subject.mouseClicked(me);
+        subject.keyPressed(ke);
 
-		// Pass
-	}
+        // Pass
+    }
 
-	@Test
-	public void testDispatchClick() {
-		final MouseEvent e = new MouseEvent(dummyEventSource, 0, 0, 0, 100,
-				100, 1, false);
-		subject.mouseClicked(e);
+    @Test
+    public void testDispatchClick() {
+        final MouseEvent e = new MouseEvent(dummyEventSource, 0, 0, 0, 100,
+                100, 1, false);
+        subject.mouseClicked(e);
 
-		verify(clickReceiver).click(100, 100);
-	}
+        verify(clickReceiver).click(100, 100);
+    }
 
-	@Test
-	public void testDispatchArrowKey() {
-		final KeyEvent e = new KeyEvent(dummyEventSource, 0, 0, 0,
-				KeyEvent.VK_DOWN, '\0');
-		subject.keyPressed(e);
+    @Test
+    public void testDispatchArrowKey() {
+        final KeyEvent e = new KeyEvent(dummyEventSource, 0, 0, 0,
+                KeyEvent.VK_DOWN, '\0');
+        subject.keyPressed(e);
 
-		verify(keyReceiver).key(KeyEvent.VK_DOWN);
-	}
+        verify(keyReceiver).key(KeyEvent.VK_DOWN);
+    }
 }
