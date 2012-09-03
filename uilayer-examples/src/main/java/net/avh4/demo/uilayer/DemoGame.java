635836622ada212f51414d4240bcd3db8e728e79
Aaron VonderHaar <gruen0aermel@gmail.com>
2012-09-03 09:50:30 -0700

    Update uilayer-examples.

--- uilayer-examples/src/main/java/net/avh4/demo/uilayer/DemoGame.java
+++ uilayer-examples/src/main/java/net/avh4/demo/uilayer/DemoGame.java
@@ -1,40 +1,42 @@
 package net.avh4.demo.uilayer;
 
-import java.awt.event.KeyEvent;
-
 import net.avh4.framework.uilayer.UI;
 import net.avh4.framework.uilayer.UILayer;
 import net.avh4.framework.uilayer.scene.Scene;
+import net.avh4.framework.uilayer.scene.SceneImage;
+import net.avh4.framework.uilayer.scene.ScenePlaceholder;
+
+import java.awt.event.KeyEvent;
 
 public class DemoGame implements UI {
 
-	public static void main(final String[] args) {
-		final DemoGame game = new DemoGame();
-		UILayer.main(game);
-	}
+    public static void main(final String[] args) {
+        final DemoGame game = new DemoGame();
+        UILayer.main(game);
+    }
 
-	private Scene s;
+    private Scene s;
 
-	public DemoGame() {
-		s = UILayer.newScene("UILayer Demo Game");
-		s.addImage(0, 0, 800, 600, "background.jpg");
-	}
+    public DemoGame() {
+        s = new Scene("UILayer Demo Game");
+        s.add(new SceneImage(0, 0, 800, 600, "background.jpg"));
+    }
 
-	@Override
-	public Scene getScene() {
-		return s;
-	}
+    @Override
+    public Scene getScene() {
+        return s;
+    }
 
-	@Override
-	public void click(final int x, final int y) {
-		s.addPlaceholder("Box", x, y, 50, 50);
-	}
+    @Override
+    public void click(final int x, final int y) {
+        s.add(new ScenePlaceholder("Box", x, y, 50, 50));
+    }
 
-	@Override
-	public void key(final int keyCode) {
-		if (keyCode == KeyEvent.VK_SPACE) {
-			s = UILayer.newScene("UILayer Demo Game");
-			s.addImage(0, 0, 800, 600, "background.jpg");
-		}
-	}
+    @Override
+    public void key(final int keyCode) {
+        if (keyCode == KeyEvent.VK_SPACE) {
+            s = new Scene("UILayer Demo Game");
+            s.add(new SceneImage(0, 0, 800, 600, "background.jpg"));
+        }
+    }
 }
