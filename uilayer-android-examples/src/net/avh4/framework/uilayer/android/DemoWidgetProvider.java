0c48b8d70fa94591f6f232933a55abe3e5374cb1
Aaron VonderHaar <gruen0aermel@gmail.com>
2012-09-20 09:03:26 -0700

    WIP: trying to make a widget.

--- uilayer-android-examples/src/net/avh4/framework/uilayer/android/DemoWidgetProvider.java
+++ uilayer-android-examples/src/net/avh4/framework/uilayer/android/DemoWidgetProvider.java
@@ -1,7 +1,19 @@
 package net.avh4.framework.uilayer.android;
 
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
 
 public class DemoWidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+        for (int appWidgetId : appWidgetIds) {
+            RemoteViews view = new RemoteViews();
+            view.apply()
+            appWidgetManager.updateAppWidget(appWidgetId, view);
+        }
 
+    }
 }
