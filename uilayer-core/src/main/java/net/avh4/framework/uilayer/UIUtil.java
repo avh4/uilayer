0fccf55803b51af1054d7b4775f16a0e00e10f7f
Aaron VonderHaar <gruen0aermel@gmail.com>
2013-02-28 21:33:34 -0800

    core: Throw an exception when UIUtil can't find the requested scene element.

--- uilayer-core/src/main/java/net/avh4/framework/uilayer/UIUtil.java
+++ uilayer-core/src/main/java/net/avh4/framework/uilayer/UIUtil.java
@@ -4,17 +4,26 @@ import net.avh4.framework.uilayer.scene.SceneElement;
 
 public abstract class UIUtil {
     public static void clickOn(UI ui, String elementName) {
-        final SceneElement element = ui.getScene().findSceneElement(elementName);
+        final SceneElement element = findSceneElement(ui, elementName);
         ui.click(element.getX() + element.getWidth() / 2, element.getY() + element.getHeight() / 2);
     }
 
     public static void clickOnTop(UI ui, String elementName) {
-        final SceneElement element = ui.getScene().findSceneElement(elementName);
+        final SceneElement element = findSceneElement(ui, elementName);
         ui.click(element.getX() + element.getWidth() / 2, element.getY() + 1);
     }
 
     public static void clickOnBottom(UI ui, String elementName) {
-        final SceneElement element = ui.getScene().findSceneElement(elementName);
+        final SceneElement element = findSceneElement(ui, elementName);
         ui.click(element.getX() + element.getWidth() / 2, element.getY() + element.getHeight() - 1);
+    }
+
+    private static SceneElement findSceneElement(UI ui, String elementName) {
+        SceneElement sceneElement = ui.getScene().findSceneElement(elementName);
+        if (sceneElement == null) {
+            throw new RuntimeException("Couldn't find SceneElement '" + elementName + "'" +
+                    " in " + ui.toString());
+        }
+        return sceneElement;
     }
 }
